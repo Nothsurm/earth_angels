594adb6195b6e259b203cf2838fab0e7b6511b66
Mike Rushton <mrush94ton@gmail.com>
2024-03-24 15:58:46 +0000

    installed Email.js dependancy

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -3,8 +3,28 @@ import { Parallax } from 'react-parallax'
 import { Checkbox, Label, TextInput, Textarea } from 'flowbite-react';
 import { MdPerson, MdEmail } from "react-icons/md";
 import Header from '../components/Header';
+import { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 export default function Contact() {
+    const form = useRef();
+
+    const sendEmail = (e) => {
+      e.preventDefault();
+  
+      emailjs
+        .sendForm(import.meta.env.VITE_SERVICE_ID, import.meta.env.VITE_TEMPLATE_ID, form.current, {
+          publicKey: VITE_PUBLIC_KEY,
+        })
+        .then(
+          () => {
+            console.log('SUCCESS!');
+          },
+          (error) => {
+            console.log('FAILED...', error.text);
+          },
+        );
+    };
   return (
     <div>
         <Header />
@@ -17,41 +37,27 @@ export default function Contact() {
             <div className="flex flex-col md:flex-row max-w-6xl gap-20 mx-auto mt-20">
                 <div className="flex-1  mr-2 ml-2 sm:ml-0 sm:mr-0">
                     <h1 className='font-bold text-3xl'>WE'RE READY, LET'S TALK.</h1>
-                    <form className='mt-14 flex flex-col gap-4'>
+                    <form className='mt-14 flex flex-col gap-4' ref={form} onSubmit={sendEmail}>
                         <div>
-                            <TextInput type="text" placeholder="Your Name" icon={MdPerson} required />
+                            <TextInput type="text" placeholder="Your Name" icon={MdPerson} name="user_name" required />
                         </div>
                         <div>
-                            <TextInput type="email" placeholder='Email Address' icon={MdEmail} required />
+                            <TextInput type="email" placeholder='Email Address' name="user_email" icon={MdEmail} required />
                         </div>
                         <div className="">
-                            <Textarea placeholder='Message' rows={4}/>
+                            <Textarea placeholder='Message' rows={4} name="message" required />
                         </div>
                         <div className="flex items-center gap-2">
                             <Checkbox id="remember" />
                             <Label htmlFor="remember">I Accept Your Terms & Conditions</Label>
                         </div>
                         <div className="">
-                        <button className='uppercase bg-orange-400 px-4 py-2 rounded-lg font-semibold hover:bg-orange-500 transition ease-in-out delay-50'>Send Message</button>
+                        <button type='submit' className='uppercase bg-zinc-400 hover:bg-zinc-600 hover:text-white px-4 py-2 rounded-lg font-semibold transition ease-in-out delay-50'>Send Message</button>
                         </div>
                     </form>
                 </div>
                 <div className="flex-1 flex flex-col gap-4 mr-2 ml-2 sm:ml-0 sm:mr-0">
-                    <h1 className='font-bold text-3xl relative'>CONTACT INFO
-                    <hr
-                        style={{
-                        background: 'orange',
-                        color: 'orange',
-                        borderColor: 'orange',
-                        height: '2px',
-                        width: '60px',
-                        position: 'absolute',
-                        top: '100%',
-                        left: '7%',
-                        transform: 'translate(-50%, -50%)'
-                        }}
-                        className='mt-6'
-                    /></h1>
+                    <h1 className='font-bold text-3xl'>CONTACT INFO</h1>
                     <h2 className='font-bold text-xl mt-10'>Address</h2>
                     <p>FemalEvolution Unipessoal Lda, UALG Gambelas PAV B1, 8005-139, FARO</p>
                     <h2 className='font-bold text-xl'>Email Us</h2>
